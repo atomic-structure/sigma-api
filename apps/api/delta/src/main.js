8fd7607fbbc7fdfb04208f46bb77f9d7f00642ef
Kevin Chwalek <kevin.chwalek@accenture.com>
2025-02-11 14:20:59 +0100

    fix(delta-api): Bundling with webpack creates executable artifact

--- apps/api/delta/src/main.js
+++ apps/api/delta/src/main.js
@@ -5,17 +5,20 @@
 
 import express from 'express';
 import * as path from 'path';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 // import { Config } from '@sigma-api/config'; Should be the target once integrated
 import { Config } from '../../../../dist/libs/config/src/index.js'; // eslint-disable-line @nx/enforce-module-boundaries
 import { deltaApiConfigSchema } from './config/schema.js';
 
+const __dirname = dirname(fileURLToPath(import.meta.url)); // TODO: Replace once migrated to TS
 
 const config = Config.getInstance('delta-api', deltaApiConfigSchema);
 const port = config.get('PORT');
 
 const app = express();
 
-app.use('/assets', express.static(path.join(import.meta.dirname, 'assets')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to delta!' });
