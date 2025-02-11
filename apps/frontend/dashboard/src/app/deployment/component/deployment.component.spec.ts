a8972ed479d4a68e1a545bcd0180623c2b147085
Kevin Chwalek <kevin.chwalek@accenture.com>
2025-02-11 23:52:44 +0100

    feat(dashboard): Fix dashboard tests

--- apps/frontend/dashboard/src/app/deployment/component/deployment.component.spec.ts
+++ apps/frontend/dashboard/src/app/deployment/component/deployment.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+
 import { DeploymentComponent } from './deployment.component';
 
 describe('DeploymentComponent', () => {
@@ -8,8 +11,13 @@ describe('DeploymentComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DeploymentComponent],
+            providers: [
+              provideHttpClient(),
+              provideHttpClientTesting(),
+            ]
     }).compileComponents();
 
+    const httpTesting = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(DeploymentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- apps/frontend/dashboard/src/app/deployment/service/deployment.service.spec.ts
+++ apps/frontend/dashboard/src/app/deployment/service/deployment.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { DeploymentService } from './deployment.service';
 
@@ -6,8 +8,14 @@ describe('DeploymentService', () => {
   let service: DeploymentService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    });
     service = TestBed.inject(DeploymentService);
+    const httpTesting = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
